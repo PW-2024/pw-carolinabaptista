b466f9e52f800f9f683dd71f6b1c642eec09ec5c
Inês Torres <inestsilva2001@hotmail.com>
2024-02-26 11:50:51 +0000

    alteracao do titulo de receita

--- src/Bolinho.js
+++ src/Bolinho.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Bolinho = () => {
   return (
     <div>
-      <h1>Bolo de Iogurte</h1>
+      <h1>Bolo de Iogurte light</h1>
       <ul>
         <li>4 ovos</li>
         <li>1/2 copo de Óleo</li>
